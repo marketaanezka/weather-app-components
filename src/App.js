beb306ce15e419a0fd7f2d8dc259c1a458aa58a7
Marketa Krucka <marketakrucka@gmail.com>
2021-10-26 00:14:13 +0200

    first phase - static css and html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "./App.css";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>My Weather App</h1>
+        <div className="weather">
+          <div className="weather__current">
+            <h2 className="weather__city" id="mesto">
+              Prague, CZ
+            </h2>
+            <div className="weather__inner weather__inner--center">
+              <div className="weather__section weather__section--temp">
+                <span className="weather__temp-value" id="teplota">
+                  --
+                </span>
+                <span className="weather__temp-unit">°C</span>
+                <div className="weather__description" id="popis">
+                  --
+                </div>
+              </div>
+              <div
+                className="weather__section weather__section--icon"
+                id="ikona"
+              >
+                --
+                {/* <img
+                  src={URL FROM OPEN WEATHER}
+                  alt="current weather icon"
+                /> */}
+              </div>
+            </div>
+            <div className="weather__inner">
+              <div className="weather__section">
+                <h3 className="weather__title">Wind</h3>
+                <div className="weather__value">
+                  <span id="vitr">--</span> km/h
+                </div>
+              </div>
+              <div className="weather__section">
+                <h3 className="weather__title">Humidity</h3>
+                <div className="weather__value">
+                  <span id="vlhkost">--</span> %
+                </div>
+              </div>
+            </div>
+            <div className="weather__inner">
+              <div className="weather__section">
+                <h3 className="weather__title">Sunrise</h3>
+                <div className="weather__value">
+                  <span id="vychod">--</span>
+                </div>
+              </div>
+              <div className="weather__section">
+                <h3 className="weather__title">Sunset</h3>
+                <div className="weather__value">
+                  <span id="zapad">--</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
