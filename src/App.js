69b536297a0866f31fca065f0dc8bcbe80e0b010
Marketa Krucka <marketakrucka@gmail.com>
2021-10-26 00:39:26 +0200

    setting state in fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-
+import React, { useState, useEffect } from "react";
 import "./App.css";
+
+const API_KEY = process.env.REACT_APP_API_ID;
+// console.log(API_KEY); // currently returns undefined not sure why
+
+const fetchCurrentWeather = (setState) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=Prague&units=metric&APPID=60990aef3d3c4f5a36b9de246444ca2f`
+  ).then((response) => {
+    if (response.ok) {
+      response.json().then((data) => {
+        console.log("fetched", data);
+        setState(data);
+      });
+    } else {
+      console.log(response);
+    }
+  });
+};
+
 const App = () => {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    fetchCurrentWeather(setWeather);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
